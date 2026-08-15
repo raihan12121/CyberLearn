36f4c4d5358e30ee00fb9e33f2abd92f187ee77d
Muhammad Raihan Molla <mdraihan2328@gmail.com>
2026-08-15 23:01:46 +0600

    fix(backend): enable allow_origin_regex in CORSMiddleware to allow credentialed requests from all Vercel domains

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,6 +30,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_origin_regex=r".*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
